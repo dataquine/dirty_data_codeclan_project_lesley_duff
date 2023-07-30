9a0d02a2b926ec345884e71b163e20fd1e617549
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-30 17:32:42 +0100

    Find out about read_rds function

---
+++ task1/data_cleaning_scripts/cleaning.R
@@ -0,0 +1,10 @@
+# Author: Lesley Duff
+# Filename: cleaning.R
+# Title: Clean decathlon data
+# Date Created: 2023-07-30
+# Description:
+#   Data provided as a file "decathlon.rds"
+
+library(tidyverse)
+
+?read_rds()
