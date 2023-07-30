89f66dbf7eda85c15e19e6329975ee4bc4666682
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-30 18:38:18 +0100

    convert row names to column athlete
    convert row names to column athlete
    data covers two competitions. Add more row/column data

--- task1/data_cleaning_scripts/cleaning.R
+++ task1/data_cleaning_scripts/cleaning.R
@@ -12,9 +12,10 @@ library(tidyverse)
 
 decathlon_data <- read_rds("raw_data/decathlon.rds")
 
-# Check column names
+dim(decathlon_data)# 41 rows, 13 columns
 head(decathlon_data)
-names(decathlon_data)
+names(decathlon_data) # Check column names
+
 # column names are mixed case and include full stops
 # Suggest janitor
 
@@ -39,18 +40,30 @@ names(clean_decathlon_data)
 # Points, points
 # Competition, competition
 
-spec(clean_decathlon_data)
+str(clean_decathlon_data)
+# All events are numeric
+# rank and points are integers
+# competition is Factor w/ 2 levels "Decastar","OlympicG"
+
+# This dataset covers results for two separate competitions
 
 # We notice that the javelin column appears to be slightly misnamed.
 # I'll rename 'javeline' to 'javelin'  before more processing
 clean_decathlon_data <- clean_decathlon_data %>% 
   rename("javelin" = "javeline")
 
-# The data is in rows with row names indicating which competitor.
+# The data is in rows with row names indicating which athlete
 # Suggest moving these names into a column instead. They are also
-# a mix of sentence case and UPPERCASE.
+# a mix of sentence case for OlympicG and UPPERCASE for Decastar event.
 
-# Turn row names into a columns
+# Turn row names into a column 'athlete'
+clean_decathlon_data <- clean_decathlon_data %>% 
+  rownames_to_column(var = "athlete") %>% 
+  
+  # Convert uppercase to title case
+  mutate(athlete = str_to_title(athlete))
+
+
 
 
 
