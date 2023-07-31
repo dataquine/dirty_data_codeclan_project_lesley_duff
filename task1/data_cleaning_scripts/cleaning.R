aafaf138838b5290b1f98662b11cfb1fcec80a0d
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-31 12:18:20 +0100

    Run styler and remove extra whitespace

--- task1/data_cleaning_scripts/cleaning.R
+++ task1/data_cleaning_scripts/cleaning.R
@@ -4,16 +4,16 @@
 # Date Created: 2023-07-30
 # Description:
 #   Data provided as in RDS format is read in, cleaned and output as a CSV file.
-# 
+#
 # Assumptions:
 #   Input: the data file is located at "raw_data/decathlon.rds"
-#   Output: we assume a folder "clean_data" has already been created. 
+#   Output: we assume a folder "clean_data" has already been created.
 #     The cleaned CSV file is output to "clean_data/decathlon.csv"
 
 library(janitor)
 library(tidyverse)
 
-#?read_rds()
+# ?read_rds()
 
 # read original decathlon dataset
 decathlon_data <- read_rds("raw_data/decathlon.rds")
@@ -21,16 +21,16 @@ decathlon_data <- read_rds("raw_data/decathlon.rds")
 # Path where cleaned CSV output will be written
 decathlon_csv_path <- "clean_data/decathlon.csv"
 
-dim(decathlon_data)# 41 rows, 13 columns
+dim(decathlon_data) # 41 rows, 13 columns
 head(decathlon_data)
 names(decathlon_data) # Check column names
 
 # column names are mixed case and include full stops
 # Suggest janitor
 
-clean_decathlon_data <- decathlon_data %>% 
+clean_decathlon_data <- decathlon_data %>%
   janitor::clean_names()
-  
+
 names(clean_decathlon_data)
 # The names contain variables matching the 10 events listed
 # 100 metres,x100m
@@ -57,19 +57,18 @@ str(clean_decathlon_data)
 # This dataset covers results for two separate competitions
 
 # A couple of  column names appear to be slightly different from expected
-# Rename 'javeline' to 'javelin'  and 
+# Rename 'javeline' to 'javelin'  and
 # 'x110m_hurdle' -> 'x110m_hurdles' before more processing
-clean_decathlon_data <- clean_decathlon_data %>% 
-  rename("javelin" = "javeline") %>% 
+clean_decathlon_data <- clean_decathlon_data %>%
+  rename("javelin" = "javeline") %>%
   rename("x110m_hurdles" = "x110m_hurdle")
-  
+
 # The data is in rows with row names indicating which athlete
 # Suggest moving these names into a column instead. They are also
 # a mix of Title case for OlympicG and UPPERCASE for Decastar event.
-decathlon_data <- clean_decathlon_data %>% 
+decathlon_data <- clean_decathlon_data %>%
   # Turn row names into a column 'athlete'
-  rownames_to_column(var = "athlete") %>% 
-  
+  rownames_to_column(var = "athlete") %>%
   # Convert e.g. original to title case so they are all the same for consistency
   mutate(athlete = str_to_title(athlete))
 
@@ -79,6 +78,3 @@ length(unique(decathlon_data$athlete))
 # Write out clean data
 # The original .rds format is converted into CSV instead for wider usage.
 write_csv(decathlon_data, decathlon_csv_path)
-
-
-
