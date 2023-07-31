7c9ce4705445c8ac340df7cdd03c455ac5dc61c1
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-07-31 17:58:14 +0100

    Initial cleaning script

---
+++ task4/data_cleaning_scripts/cleaning.R
@@ -0,0 +1,13 @@
+# Author: Lesley Duff
+# Filename: cleaning.R
+# Title: Clean Halloween Candy Data
+# Date Created: 2023-07-31
+# Description:
+#   Data provided as three spreadsheet files to be combined into one dataset.
+# boing-boing-candy-2015.xlxs, boing-boing-candy-2016.xlxs and 
+# boing-boing-candy-2017.xlxs
+#
+
+halloween_candy_files <- c("boing-boing-candy-2015.xlxs", 
+                           "boing-boing-candy-2016.xlxs", 
+                           "boing-boing-candy-2017.xlxs")
