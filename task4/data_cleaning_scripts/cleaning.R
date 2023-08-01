ca1f8af84911f01684b6d979e14b198c2d42f1d8
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-08-01 22:00:06 +0100

    add year 2015

--- task4/data_cleaning_scripts/cleaning.R
+++ task4/data_cleaning_scripts/cleaning.R
@@ -44,7 +44,8 @@ get_candy_ratings_2015 <- function(raw_data) {
     # Clean up age field - non numeric become NA
     mutate(
       age = as.numeric(age),
-      gender = NA)
+      gender = NA,
+      year = 2015)
 
   # Turn wide data into long, Analysis asks Don’t count missing values
   # So will drop NAs
@@ -169,7 +170,9 @@ examine_candy_ratings <- function(candy_ratings) {
 
 candy_ratings_2015 <- get_candy_ratings_2015(raw_2015)
 # dim(candy_ratings_2015) 
-#View(candy_ratings_2015)
+View(candy_ratings_2015)
+stop("OK")
+
 
 candy_ratings_2016 <- get_candy_ratings_2016(raw_2016)
 #dim(candy_ratings_2016)
