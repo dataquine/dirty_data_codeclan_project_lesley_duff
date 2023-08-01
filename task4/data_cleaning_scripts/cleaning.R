1740eb22b264a5eb30013409bb74d5047d6d3833
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-08-01 10:05:32 +0100

    Preliminary read in of datasets
    Read in and view all three spreadsheets

--- task4/data_cleaning_scripts/cleaning.R
+++ task4/data_cleaning_scripts/cleaning.R
@@ -8,6 +8,27 @@
 # boing-boing-candy-2017.xlxs
 #
 
-halloween_candy_files <- c("boing-boing-candy-2015.xlxs", 
-                           "boing-boing-candy-2016.xlxs", 
-                           "boing-boing-candy-2017.xlxs")
+library(readxl)
+library(tidyverse)
+
+halloween_candy_files <- c(halloween_candy_file_2015 = "boing-boing-candy-2015.xlxs", 
+                           halloween_candy_file_2016 =  "boing-boing-candy-2016.xlxs", 
+                           halloween_candy_file_2017 = "boing-boing-candy-2017.xlxs")
+
+raw_2015 <- read_excel("raw_data/boing-boing-candy-2015.xlsx")
+raw_2016 <- read_excel("raw_data/boing-boing-candy-2016.xlsx")
+raw_2017 <- read_excel("raw_data/boing-boing-candy-2017.xlsx")
+
+View(raw_2015)
+View(raw_2016)
+View(raw_2017)
+
+
+names_data_name_2015 <- names(halloween_candy_2015)
+names_data_name_2015
+
+names_data_name_2016 <- names(halloween_candy_2016)
+names_data_name_2016
+
+names_data_name_2017 <- names(halloween_candy_2017)
+names_data_name_2017 
