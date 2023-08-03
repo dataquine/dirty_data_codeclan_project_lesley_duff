070a1cdd67f6c2c911025eee89032195bd27ae88
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-08-03 18:34:03 +0100

    Add header comment

--- task4/data_cleaning_scripts/get_candy_ratings_2016.R
+++ task4/data_cleaning_scripts/get_candy_ratings_2016.R
@@ -1,10 +1,23 @@
+# Author: Lesley Duff
+# Filename: get_candy_ratings_2016.R
+# Title: Process 2015 data structure
+# Date Created: 2023-08-03
+# Description:
+#   From a data frame for 2016 restructure in to common structure used for
+# combining with other years. Produce data frame of columns:
+# age,
+# trick_or_treating,
+# gender,
+# year,
+# country,
+# candy_name,
+# candy_rating
+# candy_popularity
 
 # Helper function for standardising messy country names
 source("data_cleaning_scripts/clean_country.R")
 
 # Get candy ratings from raw data for 2016
-# Produce dataframe of columns age, trick_or_treating, gender, year, country
-# candy_name, candy_rating and candy_popularity
 get_candy_ratings_2016 <- function(raw_data) {
   candy_ratings <- raw_data %>%
     select(
@@ -15,10 +28,10 @@ get_candy_ratings_2016 <- function(raw_data) {
       country = `Which country do you live in?`,
       "[100 Grand Bar]":"[York Peppermint Patties]"
     )
-  
+
   # Do search and replace on country column
   candy_ratings <- clean_country(candy_ratings)
-  
+
   # Clean up age field - non numeric become NA
   candy_ratings <- candy_ratings %>%
     mutate(
@@ -29,16 +42,16 @@ get_candy_ratings_2016 <- function(raw_data) {
     ) %>%
     # Move column to same order as 2015
     relocate(country, .after = year)
-  
+
   # Turn wide data into long, Analysis asks Don’t count missing values
   # So will drop NAs
   candy_ratings_2016_long <- candy_ratings %>%
     pivot_longer("[100 Grand Bar]":"[York Peppermint Patties]",
-                 names_to = "candy_name",
-                 # Remove leading and trailing square brackets
-                 names_pattern = "^\\[(.*)\\]$",
-                 values_to = "candy_rating",
-                 values_drop_na = TRUE
+      names_to = "candy_name",
+      # Remove leading and trailing square brackets
+      names_pattern = "^\\[(.*)\\]$",
+      values_to = "candy_rating",
+      values_drop_na = TRUE
     ) %>%
     mutate(candy_popularity = case_when(
       candy_rating == "DESPAIR" ~ -1,
@@ -48,7 +61,7 @@ get_candy_ratings_2016 <- function(raw_data) {
     # Check that we don't have NAs in pivoted columns
     verify(!is.na(candy_name)) %>%
     verify(!is.na(candy_rating))
-  
+
   # View(candy_ratings_2016_long)
   return(candy_ratings_2016_long)
 }
